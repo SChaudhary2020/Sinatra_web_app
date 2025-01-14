c115e8ab83ca4df5408cd9a507adb83dccbff8d0
Sarita Chaudhary <71405330+SChaudhary2020@users.noreply.github.com>
2025-01-14 22:16:18 +0000

    sinatra app

--- app.rb
+++ app.rb
@@ -1,9 +1,28 @@
 require "sinatra"
 require "sinatra/reloader"
+require "active_support/all"
 
 get("/") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
+  <h1>Welcome to Chicago!</h1>
   "
+end
+get("/login/page") do
+  erb(:loginpage)
+
+end
+
+get("/login/result") do
+   @username = params.fetch(:password)
+   @password = params.fetch(:username)
+
+   if @username == "sinatra" and @password =="password"
+    "Welcome, #{@username}"
+   else
+    "Invalid username, #{@password}!"
+   end
+  erb(:login_result)
+end
+get("/home/page") do
+  erb(:homepage)
 end
